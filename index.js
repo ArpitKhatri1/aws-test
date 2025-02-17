26d14bf5ff1486e5bc024a3daa35124d25968cdc
ArpitKhatri1 <arpitkumarkhatri@gmail.com>
2025-02-17 15:15:03 +0530

    final

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-app.get("/", (res, req) => {
+app.get("/", (req, res) => {
   res.json({
     message: "The instance is running fine",
   });
